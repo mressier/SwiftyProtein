104d8eec255119c48f47ff6f8eb0bc91f19e5d44
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2020-04-27 11:55:49 +0200

    Create a SectionSource for collection or table view

---
+++ SwiftyProtein/SwiftyProtein/Controllers/LigandList/Model/LigandsAppList.swift
@@ -0,0 +1,9 @@
+//
+//  LigandsAppList.swift
+//  SwiftyProtein
+//
+//  Created by Mathilde Ressier on 27/04/2020.
+//  Copyright © 2020 Mathilde Ressier. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftyProtein/SwiftyProtein/Shared/Tools/TableViewUtils/SectionSource.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+/*******************************************************************************
+ * SectionSource
+ *
+ * Common source for table view based on object ( `[section: String, content: [Element]] `)
+ *
+ ******************************************************************************/
+class SectionSource<Section, Element: Equatable>: CollectionSource {
+
+  typealias ElementType = Element
+
+  typealias SectionSourceContent = (section: Section, content: [Element])
+
+  //----------------------------------------------------------------------------
+  // MARK: - Properties
+  //----------------------------------------------------------------------------
+
+  /******************** Content ********************/
+
+  var elements = [SectionSourceContent]()
+
+  //----------------------------------------------------------------------------
+  // MARK: - Data source
+  //----------------------------------------------------------------------------
+
+  func element(at indexPath: IndexPath) -> Element {
+    return elements(inSection: indexPath.section)[indexPath.row]
+  }
+
+  func elements(inSection section: Int) -> [Element] {
+    return elements[section].content
+  }
+
+  /******************** Index Path ********************/
+
+  func indexPath(for element: Element) -> IndexPath? {
+    for (sectionIndex, section) in elements.enumerated() {
+      if let rowIndex = section.content.firstIndex(where: { $0 == element }) {
+        return IndexPath(row: rowIndex, section: sectionIndex)
+      }
+    }
+    return nil
+  }
+
+  func indexPaths(for elements: [Element]) -> [IndexPath] {
+    return elements.compactMap() { indexPath(for: $0) }
+  }
+
+  /******************** Section Key ********************/
+
+  func sectionKey(at indexPath: IndexPath) -> Section {
+    return sectionKey(at: indexPath.section)
+  }
+
+  func sectionKey(at index: Int) -> Section {
+    return elements[index].section
+  }
+
+}
