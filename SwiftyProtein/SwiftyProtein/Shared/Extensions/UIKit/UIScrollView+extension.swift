0c98cca590aeb65e15b5c7cf099a806e99e33f3a
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2020-04-29 12:01:22 +0200

    Rename some files on extensions

--- SwiftyProtein/SwiftyProtein/Shared/Extensions/UIKit/UIScrollView+extension.swift
+++ SwiftyProtein/SwiftyProtein/Shared/Extensions/UIKit/UIScrollView+extension.swift
@@ -1,9 +1,15 @@
-//
-//  UIScrollView+extension.swift
-//  SwiftyProtein
-//
-//  Created by Mathilde Ressier on 28/04/2020.
-//  Copyright © 2020 Mathilde Ressier. All rights reserved.
-//
+import UIKit
 
-import Foundation
+extension UIScrollView {
+
+  /// Return content offset as a percent between 0 and 1 of the total scroll view
+  var contentOffsetPercent: CGSize {
+    let height = contentSize.height - visibleSize.height
+    let width = contentSize.width - visibleSize.width
+
+    let percentHeight = height != 0 ? CGFloat(contentOffset.y) / height : 0.0
+    let percentWidth = width != 0 ? CGFloat(contentOffset.x) / width : 0.0
+    return CGSize(width: percentWidth, height: percentHeight)
+  }
+
+}
